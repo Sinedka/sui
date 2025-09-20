32f10e01d733eb9c26e3eae96d41953f0697b862
Sinedka <dennnn8888@gmail.com>
2025-09-20 12:05:01 +0300

    many changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+
 const int Times = 10;
 
-int main(){
-    for(int i = 0; i < Times; i++) {
-      std::cout << "Hello world\n";
-    }
+class A {
+  int test_;
+};
+
+
+int main() {
+  int a_ = 10;
+  for(int i = 0; i < Times; i++) {
+    std::cout << "Hello world\n";
+  }
 }
